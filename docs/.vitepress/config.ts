cffde151970ae436e00c74d4afc92229d85f53a4
Andrey <andrey.s.yura@gmail.com>
2025-11-13 18:41:52 +0200

    Temporarily remove CNAME and set base for GitHub Pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'GreenCome Mapping Wiki',
   description: 'Documentation for GreenCome Mapping MLO Projects',
   lang: 'en-US',
-  base: '/',
+  base: '/website_docs/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'preconnect', href: 'https://github.com' }],
